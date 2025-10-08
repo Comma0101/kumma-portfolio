ece05c54571c68a13a2350e016ae78f9793e28e6
Kuma <kuma.@Kumas-MacBook-Pro.local>
2025-10-08 16:38:05 -0700

    feat: Configure project for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/pocket/:path*',
-        destination: 'http://127.0.0.1:5000/pocket/:path*',
-      },
-    ]
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+  images: {
+    unoptimized: true,
   },
-};
+  basePath: "/kumma-portfolio",
+  assetPrefix: "/kumma-portfolio",
+}
 
-export default nextConfig;
+module.exports = nextConfig
