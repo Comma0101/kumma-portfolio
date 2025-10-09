9bfe53daa96182488da38cb0818831611af90f21
Kuma <kuma.@Kumas-MacBook-Pro.local>
2025-10-08 18:08:49 -0700

    fix: Re-enable static export

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  output: 'export',
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
